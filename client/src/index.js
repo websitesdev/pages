72699572c7555b4abae8120c963329779ca22e1a
websitesdev <website_dev@yahoo.com>
2024-02-26 08:44:23 +0200

    gh

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import App from './App';
 import './styles/index.scss';
-import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import { ReactDOM } from 'react';
 
 
 
-const container = document.getElementById('root');
+/*const container = document.getElementById('root');
 const root = createRoot(container); 
-root.render(<App tab="root" />);
+root.render(<App tab="root" />);*/
+
+ReactDOM.render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
